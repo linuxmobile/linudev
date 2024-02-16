a649bf60afd4b055af566f0bb4c762569c445111
Braian A. Diez <bdiez19@gmail.com>
2024-02-15 23:32:14 -0300

    init: starting project w/ nuxt & unocss

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup>
+useHead({
+  bodyAttrs: {
+    class: 'bg-black text-white'
+  }
+})
+
+useSeoMeta({
+  title: 'LinuDev - Un blog sobre programación para novatos',
+  description:
+    'Un blog sobre programación para novatos, javascript, react, rust, bash, y sobre todo linux.',
+  ogTitle: 'LinuDev - Un blog sobre programación para novatos',
+  ogDescription:
+    'Un blog sobre programación para novatos, javascript, react, rust, bash, y sobre todo linux.',
+  twitterCard: 'summary_large_image'
+})
+
+</script>
+<template>
+</template>
