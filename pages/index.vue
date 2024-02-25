b77a1533efc0d622ccae0d198561f6f47226c2f0
Braian A. Diez <bdiez19@gmail.com>
2024-02-25 18:49:08 -0300

    fix: improving mediaqueries

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 </script>
 <template>
-  <main class="px-4 mx-auto grid grid-cols-1 gap-y-20 md:max-w-2xl xl:max-w-7xl">
+  <main class="px-4 mx-auto grid grid-cols-1 gap-y-20 md:max-w-2xl lg:max-w-5xl xl:max-w-7xl">
     <HeroSection />
     <ProjectSection />
     <PinnedArticles />
