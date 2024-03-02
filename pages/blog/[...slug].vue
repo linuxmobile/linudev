51db6e7e67ad9962800a5e54a9ef9f056983809d
Braian A. Diez <bdiez19@gmail.com>
2024-03-02 20:50:13 -0300

    feat: added TOC

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,9 +1,7 @@
 <script setup>
-
+const { toc } = useContent()
 const route = useRoute()
 const article = await queryContent('blog').where({_path: route.path}).findOne();
-
-console.log(article)
 
 useSeoMeta({
   title: article.title,
@@ -36,15 +34,22 @@ defineOgImageComponent(
 
 </script>
 <template>
-  <main class="pt-34 px-4 xl:max-w-7xl mx-auto grid grid-cols-1 gap-y-8">
+  <main class="pt-34 px-4 xl:max-w-7xl mx-auto grid grid-cols-1 gap-x-3 gap-y-8">
     <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
-      <ContentRenderer :value="data">
-        <header>
+      <ContentRenderer :value="data" class="grid grid-cols-8">
+        <header class="col-span-8">
           <h1 class="text-5xl font-medium font-display ">{{data.title}}</h1>
           <p class="font-medium opacity-70 line-clamp-3">{{data.description}}</p>
           <p class="text-sm opacity-60">{{ formatDate(data.date) }}</p>
         </header>
-        <ContentRendererMarkdown :value="data" class="prose-sm"/>
+        <ContentRendererMarkdown :value="data" class="prose-sm text-sm md:text-md lg:text-lg col-span-4 max-w-max"/>
+        <ul v-if="toc && toc.links" class="col-span-4 pt-16 sticky top-15 max-h-[40vh] overflow-y-auto">
+          <li v-for="link in toc.links" :key="link.text">
+            <a :href="`#${link.id}`" class="text-xl text-[#ECAB43]">
+              {{ link.text }}
+            </a>
+          </li>
+        </ul>
       </ContentRenderer>
     </ContentQuery>
   </main>
