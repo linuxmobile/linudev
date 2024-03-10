28e0053bf5ebc273837d53abde1492c63e420599
Braian A. Diez <bdiez19@gmail.com>
2024-03-10 12:44:42 -0300

    feat: improved response

--- pages/blog/[[page]].vue
+++ pages/blog/[[page]].vue
@@ -42,15 +42,13 @@ const { formatTimeAgo } = useFormatTime()
   <main class="px-4 xl:max-w-7xl mx-auto grid grid-cols-1 pt-34 gap-y-4">
     <h2 class="font-black font-display text-5xl">Artículos Recientes</h2>
     <section class="grid gap-3 xl:grid-cols-2">
-      <DelayHydration>
-        <article v-for="article in latestArticles" :key="article._path" class="grid gap-y-4 rounded-lg px-3 py-2 relative bg-white/5 border border-white/10">
-          <NuxtLink :to="article._path" class="px-4 py-3 w-full block flex flex-col gap-y-2">
-            <h2 class="font-display font-black text-2xl">{{ article.title }}</h2>
-            <p class="text-gray-300 font-medium line-clamp-2">{{ article.description }}</p>
-            <p class="text-gray-400 text-sm">{{ formatTimeAgo(new Date(article.date)) }}</p>
-          </NuxtLink>
-        </article>
-      </DelayHydration>
+      <article v-for="article in latestArticles" :key="article._path" class="grid gap-y-4 rounded-lg px-3 py-2 relative bg-white/5 border border-white/10">
+        <NuxtLink :to="article._path" class="px-4 py-3 w-full block flex flex-col gap-y-2">
+          <h2 class="font-display font-black text-2xl">{{ article.title }}</h2>
+          <p class="text-gray-300 font-medium line-clamp-2">{{ article.description }}</p>
+          <p class="text-gray-400 text-sm">{{ formatTimeAgo(new Date(article.date)) }}</p>
+        </NuxtLink>
+      </article>
     </section>
     <BlogPagination :limit="limit" :page="page" :count="articlesCount" />
   </main>
