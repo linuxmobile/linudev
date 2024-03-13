4ec1187b12fb7e8a5433dd73ed9678715eddbd83
Braian A. Diez <bdiez19@gmail.com>
2024-03-12 21:52:30 -0300

    fix: adding max-w

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -42,12 +42,12 @@ defineOgImageComponent(
   <main class="pt-34 px-4 xl:max-w-7xl mx-auto grid grid-cols-1 gap-x-3 gap-y-8">
     <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
       <ContentRenderer :value="data" class="grid grid-cols-8">
-        <header class="col-span-8">
+        <header class="col-span-8 max-w-5xl">
           <h1 class="text-5xl font-medium font-display ">{{data.title}}</h1>
           <p class="font-medium opacity-70 line-clamp-3">{{data.description}}</p>
           <p class="text-sm opacity-60">{{ formatDate(data.date) }}</p>
         </header>
-        <ContentRendererMarkdown :value="data" class="prose-sm text-sm md:text-md lg:text-lg col-span-4 max-w-max"/>
+        <ContentRendererMarkdown :value="data" class="prose-sm text-sm md:text-md lg:text-lg col-span-4 max-w-4xl"/>
         <ul v-if="toc && toc.links" class="col-span-4 pt-16 sticky top-15 max-h-[40vh] overflow-y-auto">
           <li v-for="link in toc.links" :key="link.text">
             <a :href="`#${link.id}`" class="text-xl text-[#ECAB43]">
